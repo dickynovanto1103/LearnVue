e787a58309774756d59727ad2d092ab8251d2fd1
dickynovanto1103 <dickynovanto1103@gmail.com>
2020-03-24 23:15:46 +0800

    Add code to learn about loop

---
+++ loop/README.md
@@ -0,0 +1,5 @@
+### Loop
+1. We learnt about looping
+2. Keyword v-for
+3. Example: <div v-for="name in names"> {{ name }} </div>
+4. It can handle simple to complex object, just access it like an object using dot (.) --> person.name
---
+++ loop/main.js
@@ -0,0 +1,22 @@
+var app = new Vue({
+    el: "#app",
+    data: {
+        name: "dicky",
+        family: ["father", "mother", "brother"],
+        names: ["papa", "mama", "adik"],
+        details: [
+            {
+                name: "papa",
+                hobby: "sleep"
+            },
+            {
+                name: "mama",
+                hobby: "cooking"
+            },
+            {
+                name: "adik",
+                hobby: "breath"
+            }
+        ]
+    }
+})
