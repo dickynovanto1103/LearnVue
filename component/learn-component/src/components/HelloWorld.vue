4efecf4f4ec3dcc31db6d568db903dbc2f761b40
Dicky Novanto <dickynovanto1103@gmail.com>
2020-04-17 23:59:42 +0800

    Learn component vue

---
+++ component/learn-component/src/components/HelloWorld.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="hello">
+    <div>{{ msg }}</div>
+    <button v-on:click="count++">You clicked me {{ count }} times</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  },
+  data: function() {
+    return {
+      count: 0,  
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
